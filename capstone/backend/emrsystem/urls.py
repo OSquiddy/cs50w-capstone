40d3a5844b4cf66c2c1e9197e140755bb9815ca6
OSquiddy <siddiqui.omar21@gmail.com>
2021-07-18 23:48:25 +0530

    Added views for patientList, patientInfo & report

--- capstone/backend/emrsystem/urls.py
+++ capstone/backend/emrsystem/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     # path("", views.index, name="index"),
     path("hello", views.hello_world, name="hello"),
     path("userList", views.users, name="users"),
+    path("patientList", views.patients, name="patients"),
+    path("p/<int:id>", views.patientInfo, name="patientInfo"),
+    path("p/<int:id>/v/<int:visitNumber>", views.getReport, name="report"),
     # path('', include(vue_urls)),
     
     # api urls
--- capstone/backend/emrsystem/views.py
+++ capstone/backend/emrsystem/views.py
@@ -1,3 +1,4 @@
+from warnings import catch_warnings
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
@@ -5,7 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import *
 from .serializers import *
@@ -22,6 +23,56 @@ def users(request):
     serializer = DoctorSerializer(userList, many=True)
     return Response({ "userList": serializer.data, "current-user": user.data })
 
+@api_view(['GET', 'POST'])
+def patients(request):
+    patientList = Patient.objects.all().order_by('first_name')
+    serializer = PatientSerializer(patientList, many=True)
+    return Response({"patientList": serializer.data})
+
+@api_view(['GET'])
+def patientInfo(request, id):
+    patientInfo = Patient.objects.get(id=id)
+    serializer = PatientSerializer(patientInfo)
+    return Response({ "patientInfo": serializer.data })
+
+@api_view(['GET'])
+def getReport(request, id, visitNumber):
+    patient = Patient.objects.get(id=id)
+    print(patient)
+    try:
+        examination = Examination.objects.get(visit__patient=id, visit__visit_number=visitNumber)
+        examinationSerializer = ExaminationSerializer(examination)
+        examination = examinationSerializer.data
+    except ObjectDoesNotExist:
+        examinationSerializer = {}
+        examination = examinationSerializer
+    
+    try:
+        pastHistory = PastHistory.objects.get(visit__patient=id, visit__visit_number=visitNumber)
+        pastHistorySerializer = PastHistorySerializer(pastHistory)
+        pastHistory = pastHistorySerializer.data
+    except ObjectDoesNotExist:
+        pastHistorySerializer = {}
+        pastHistory = pastHistorySerializer
+
+    if patient.sex == 'f':
+        try:
+            gynecHistory = GynecHistory.objects.get(visit__patient=id, visit__visit_number=visitNumber)
+            gynecHistorySerializer = GynecHistorySerializer(gynecHistory)
+            gynecHistory = gynecHistorySerializer.data
+        except ObjectDoesNotExist:
+            gynecHistorySerializer = {}
+            gynecHistory = gynecHistorySerializer
+    else:
+        gynecHistorySerializer = {}
+        gynecHistory = gynecHistorySerializer
+    
+    return Response({ 
+        "examination": examination,
+        "pastHistory": pastHistory,
+        "gynecHistory": gynecHistory
+    })
+    
 
 
 
