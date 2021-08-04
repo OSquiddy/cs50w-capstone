b0e943b9dd49acdb3b89796d7beb2a3f18b76bde
OSquiddy <siddiqui.omar21@gmail.com>
2021-08-05 04:33:22 +0530

    Added filt.Pats, apptList, filt.ApptList API urls

--- capstone/backend/emrsystem/urls.py
+++ capstone/backend/emrsystem/urls.py
@@ -11,8 +11,11 @@ urlpatterns = [
     path("hello", views.hello_world, name="hello"),
     path("userList", views.users, name="users"),
     path("patientList", views.patients, name="patients"),
+    path("patientList/<str:query>", views.filteredPatients, name="filteredPatients"),
     path("p/<int:id>", views.patientInfo, name="patientInfo"),
     path("p/<int:id>/v/<int:visitNumber>", views.getReport, name="report"),
+    path("appointmentsList", views.appointmentsList, name="appointmentsList"),
+    path("appointmentsList/<str:query>", views.filteredAppointments, name="filteredAppointments"),
     # path('', include(vue_urls)),
     
     # api urls
