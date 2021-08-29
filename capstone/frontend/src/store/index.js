6418a5a5a00a49a6f845e7eb52e1809956ec3411
OSquiddy <siddiqui.omar21@gmail.com>
2021-08-29 15:29:57 +0530

    Toggle code-> sidepanel collapse. Forgot to commit

--- capstone/frontend/src/store/index.js
+++ capstone/frontend/src/store/index.js
@@ -6,11 +6,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    isMobile: false
-  },
-  mutations: {
+    isMobile: false,
+    isCollapsed: false,
+    selectedTab: 0
   },
   actions: {
+    toggleCollapse({ commit }) {
+      commit('TOGGLE_COLLAPSE')
+    },
+    updateSelectedTab({ commit }, value) {
+      commit('UPDATE_SELECTED_TAB', value)
+    }
+  },
+  mutations: {
+    TOGGLE_COLLAPSE(state) {
+      state.isCollapsed = !state.isCollapsed
+      sessionStorage.setItem('isCollapsed', state.isCollapsed)
+    },
+    UPDATE_SELECTED_TAB(state, payload) {
+      state.selectedTab = payload
+      sessionStorage.setItem('selectedTab', payload)
+    }
   },
   modules: {
     search
