c6096245ab141fdc661c52bcb55f76514eb2cc09
OSquiddy <siddiqui.omar21@gmail.com>
2021-08-29 15:22:02 +0530

    Created pat. dir table on desktop. Non interactive

--- capstone/frontend/src/views/PatientDirectory/PatientDirectory.vue
+++ capstone/frontend/src/views/PatientDirectory/PatientDirectory.vue
@@ -1,42 +1,44 @@
 <template>
   <div class="patient-directory-main">
-    <div class="container patients-list">
-      <template v-for="patient in patientsList">
-        <router-link :to="'/p/' + patient.id" :key="patient.id">
-        <div class="row patient-small-container">
-          <div class="col-3 patient-photo"></div>
-          <div class="col-9 patient-info">
-            <h3 class="patient-name">{{patient.first_name}} {{patient.last_name}}</h3>
-            <div class="row patient-details-row">
-              <div class="col patient-details">ID: {{patient.id}}</div>
-              <div class="col patient-details">Age: {{patient.age}}yrs</div>
-            </div>
-            <div class="row patient-details-row">
-              <div class="col patient-details">Blood Type: {{patient.age}}</div>
-              <div class="col patient-details">Gender: {{patient.sex}}</div>
-            </div>
-            <div class="row patient-details-row">
-              <div class="col patient-details">Mobile: {{patient.Phone_Number}}</div>
-            </div>
-          </div>
-        </div>
-        </router-link>
-      </template>
+    <input type="text" class="search" placeholder="Search by name or id" v-model="keyword">
+    <div class="page-title">Patients</div>
+    <div class="directory-container">
+      <table class="patients-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>LAST VISIT</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="patient in patientsList" :key="patient.id">
+            <td>{{patient.id}}</td>
+            <td>
+              <div class="patient-name">
+                <div class="patient-img"><img src="" alt=""></div>
+                {{patient.fullname}}
+              </div>
+            </td>
+            <td>{{patient.email}}</td>
+            <td>04-06-2021</td>
+          </tr>
+          </tbody>
+      </table>
     </div>
-    <button class="new-appointment">
-      <img src="../../assets/plus.svg" alt="add-symbol">
-    </button>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
 import axios from 'axios'
-import { mapState } from 'vuex'
-
+import { debounce } from '../../util/util'
 export default {
   name: 'PatientDirectory',
   data() {
     return {
+      keyword: null,
       patientsList: []
     }
   },
@@ -44,6 +46,9 @@ export default {
     ...mapState('search', ['searchKeyword'])
   },
   watch: {
+    keyword () {
+      this.debounceInput()
+    },
     searchKeyword() {
       if (this.searchKeyword !== '') {
         this.getFilteredPatientsList()
@@ -56,72 +61,77 @@ export default {
     this.getPatientsList()
   },
   methods: {
+    ...mapActions('search', ['updateSearchKeyWord']),
+    debounceInput: debounce(function (e) {
+      this.updateSearchKeyWord(this.keyword)
+    }, 250),
     async getPatientsList () {
-      const list = await axios.get(process.env.VUE_APP_API_URL + '/patientList')
+      const list = await axios.get(process.env.VUE_APP_API_URL + '/patientList/id')
       this.patientsList = list.data.patientList
     },
     async getFilteredPatientsList () {
-      const list = await axios.get(process.env.VUE_APP_API_URL + '/patientList/' + this.searchKeyword)
+      const list = await axios.get(process.env.VUE_APP_API_URL + '/patientList/id/' + this.searchKeyword)
       this.patientsList = list.data.patientList
     }
   }
+
 }
 </script>
 
 <style lang="scss" scoped>
-.patient-directory-main {
-  padding: 20px 20px 55px;
-  height: calc(100vh - 197px);
-  overflow-y: scroll;
-  .patients-list {
-    .patient-small-container {
-      display: flex;
-      background: #f4f4f4;
-      border-radius: 20px;
-      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2);
-      padding: 15px 5px;
-      margin-bottom: 20px;
-      // margin: 0 20px;
-      // &:first-of-type {
-      //   margin-top: 20px;
-      // }
-      .patient-photo {
-        background-color: #c4c4c4;
-        border-radius: 50%;
-        width: 22%;
-        aspect-ratio: 1;
-        margin-left: auto;
-        align-self: flex-start;
-      }
-      .patient-info {
-        // padding-bottom: 20px;
-        padding-left: 1rem;
-        .patient-name {
-          font-size: 1.125rem;
-        }
-        .patient-details-row {
-          margin-bottom: 3px;
-          &:last-of-type {
-            margin-bottom: 0;
-          }
-          .patient-details {
-            font-size: 0.625rem;
-          }
-        }
-      }
+.directory-container {
+  background-color: var(--background-primary);
+  padding: 1rem;
+  margin-top: 15px;
+  border-radius: 0.75rem;
+  min-height: calc(100vh - 200px);
+  margin-bottom: 20px;
+}
+
+.search {
+  border: 1px solid #99A1A6;
+  background: transparent;
+  border-radius: 0.375rem;
+  padding: 5px 10px;
+  width: 300px;
+  margin-top: 30px;
+}
+
+.page-title {
+  margin-top: 15px;
+}
+
+.patients-table {
+  width: 100%;
+  th, td {
+    text-align: center;
+    padding: 0.625rem 0;
+    font-weight: 400;
+  }
+  th {
+    font-size: 1.125rem;
+  }
+  td:nth-child(2) {
+    text-align: left;
+  }
+  tr {
+    border-bottom: 1px solid #BBB;
+  }
+  tbody tr:last-child {
+    border-bottom: none;
+  }
+  .patient-name {
+    display: flex;
+    align-items: center;
+    padding: 0.25rem 0.5rem;
+    .patient-img {
+      background: var(--medium-gray);
+      width: 2rem;
+      height: 2rem;
+      border-radius: 50%;
+      margin-right: 15px;
+      margin-left: 20%;
     }
   }
-  .new-appointment {
-  background-color: #353535;
-  border-radius: 50%;
-  aspect-ratio: 1;
-  border: none;
-  position: absolute;
-  bottom: 30px;
-  right: 35px;
-  img {
-    padding: 8px;
-  }
-}
 }
 </style>
