343db6cb9be87ced94c2726f7eaf8da803efedf7
OSquiddy <siddiqui.omar21@gmail.com>
2021-09-04 17:23:42 +0530

    Local tabs for certain pages

---
+++ capstone/frontend/src/components/LocalTabs.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="local-tabs-container">
+    <ul class="tablist">
+      <li class="tab active" @click="selectTab(0, $event)">Upcoming</li>
+      <li class="tab" @click="selectTab(1, $event)">History</li>
+      <li class="tab" @click="selectTab(2, $event)">Reports</li>
+      <li class="tab" @click="selectTab(3, $event)">Notes</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LocalTabs',
+  data () {
+    return {
+      selectedTab: 0,
+      tabPaths: ['upcoming', 'history', 'reports', 'notes']
+    }
+  },
+  watch: {
+    selectedTab (newValue, oldValue) {
+      if (this.$route.path !== this.tabPaths[newValue]) {
+        this.$router.push({ name: this.tabPaths[newValue] })
+      }
+      document.querySelectorAll('.tab')[oldValue].classList.remove('active')
+    }
+  },
+  mounted () {
+    // console.log(this.$router)
+  },
+  methods: {
+    selectTab (index, event) {
+      this.selectedTab = index
+      const elem = event.target
+      elem.classList.add('active')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.local-tabs-container {
+  display: flex;
+  background: white;
+}
+
+.tablist {
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  .tab {
+    margin: 0 1.5rem;
+    padding: 0.5rem 0;
+    // border: 1px solid;
+    cursor: pointer;
+  }
+}
+
+.active {
+  color: var(--focus-blue);
+  border-bottom: 4px solid var(--focus-blue);
+}
+</style>
