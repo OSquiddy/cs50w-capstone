cf92686442152c8600fa334eb90d48f95fc9fe52
OSquiddy <siddiqui.omar21@gmail.com>
2021-09-04 17:15:59 +0530

    Got rid of sidepanel selectedTab logic from store

--- capstone/frontend/src/store/index.js
+++ capstone/frontend/src/store/index.js
@@ -7,25 +7,17 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     isMobile: false,
-    isCollapsed: false,
-    selectedTab: 0
+    isCollapsed: false
   },
   actions: {
     toggleCollapse({ commit }) {
       commit('TOGGLE_COLLAPSE')
-    },
-    updateSelectedTab({ commit }, value) {
-      commit('UPDATE_SELECTED_TAB', value)
     }
   },
   mutations: {
     TOGGLE_COLLAPSE(state) {
       state.isCollapsed = !state.isCollapsed
       sessionStorage.setItem('isCollapsed', state.isCollapsed)
-    },
-    UPDATE_SELECTED_TAB(state, payload) {
-      state.selectedTab = payload
-      sessionStorage.setItem('selectedTab', payload)
     }
   },
   modules: {
