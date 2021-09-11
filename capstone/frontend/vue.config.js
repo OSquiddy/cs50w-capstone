80ee04f2b82b2e99bcd20514cac2a76a95bb6951
OSquiddy <siddiqui.omar21@gmail.com>
2021-09-12 03:47:28 +0530

    New vue.config

--- capstone/frontend/vue.config.js
+++ capstone/frontend/vue.config.js
@@ -8,16 +8,23 @@ module.exports = {
       'public': 'http://localhost:8080/',
       'host': 'localhost',
       'disableHostCheck': true
-    }
-    // 'chainWebpack': config => {
-    //   config.module
-    //     .rule('vue')
-    //     .use('vue-svg-inline-loader')
-    //     .loader('vue-svg-inline-loader')
-    //     .options({ /* ... */ })
-  
+    },
+    'chainWebpack': config => {
+      // config.module
+      //   .rule('vue')
+      //   .use('vue-svg-inline-loader')
+      //   .loader('vue-svg-inline-loader')
+      //   .options({ /* ... */ })
+
+      // config.module.rule('pdf')
+      // .test(/\.(pdf)(\?.*)?$/)
+      // .use('file-loader')
+      //   .loader('file-loader')
+      //   .options({
+      //     name: 'assets/[name].[hash:8].[ext]'
+      //   })
       // config.output.chunkFilename('[name].[contenthash].js')
       // config.optimization.moduleIds = 'deterministic'
-    // }
+    }
   
   }
