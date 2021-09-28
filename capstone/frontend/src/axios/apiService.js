91b5f6fdfe617a43cbafa58e2340b3f4c78d997a
OSquiddy <siddiqui.omar21@gmail.com>
2021-09-28 10:43:56 +0530

    Delete unnecessary apiService file

--- capstone/frontend/src/axios/apiService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const VUE_APP_API_URL = 'http://localhost:8000'
-
-export class APIService {
-  async getUsers () {
-    const url = `${VUE_APP_API_URL}/api/getUsers/`
-    return await axios.get(url)
-  }
-}
