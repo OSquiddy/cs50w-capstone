0e5f935ef6e2826ff5fec5392094861ddb85f68e
OSquiddy <siddiqui.omar21@gmail.com>
2021-09-28 10:50:33 +0530

    NewAppointment styling v1

--- capstone/frontend/src/views/NewAppointment/NewAppointment.vue
+++ capstone/frontend/src/views/NewAppointment/NewAppointment.vue
@@ -1,20 +1,54 @@
 <template>
   <div class="add-appt-main">
-    <form action="" method="post" @submit.prevent="createAppointment">
-      <button>Cancel</button>
-      <button type="submit">Submit</button>
-      <label for="patient">Patient:</label>
-      <SearchableDropdown :id="'patient'" :placeholder="'Patient Name'" :dataList="patientsList" :isPatient="true"/>
-      <label for="date">Date</label>
-      <datetime v-model="date" type="date" :format="'ccc, MMM dd, yyyy'"></datetime>
-      <label for="patient">Time:</label>
-      <datetime type="time" v-model="time1" use12-hour :format="'hh:mm a'"></datetime>
-      <datetime type="time" v-model="time2" use12-hour :format="'hh:mm a'"></datetime>
-      <label for="doctor">Doctor</label>
-      <SearchableDropdown :id="'doctor'" :placeholder="'Doctor Name'" :dataList="doctorsList"/>
-      <label for="payment">Payment:</label>
-      <input type="number" name="payment" id="payment" v-model="payment">
-    </form>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-4">
+          <div class="section-header">Add Appointment</div>
+          <form action="" method="post" @submit.prevent="createAppointment">
+            <div class="section-input">
+              <label for="patient">
+                  <!-- <img src="../../assets/patient.svg" alt="patient-icon" class="patient-icon" /> -->
+                Patient :</label>
+              <div>
+                <SearchableDropdown :id="'patient'" :placeholder="'Patient Name'" :dataList="patientsList" :isPatient="true"/>
+              </div>
+            </div>
+            <div class="section-input">
+              <label for="date">
+                  <!-- <img src="../../assets/clock.svg" alt="patient-icon" class="patient-icon" /> -->
+                Date :</label>
+              <datetime v-model="date" type="date" :format="'ccc, MMM dd, yyyy'"></datetime>
+            </div>
+            <div class="section-input">
+              <label for="patient">Time :</label>
+              <div class="time-input-group">
+                <datetime type="time" v-model="time1" use12-hour :format="'hh:mm a'"></datetime>
+                <!-- <img src="../../assets/right-arrow.svg" alt="right-arrow" /> -->
+                <datetime type="time" v-model="time2" use12-hour :format="'hh:mm a'"></datetime>
+              </div>
+            </div>
+            <div class="section-input">
+              <label for="doctor">
+                  <!-- <img src="../../assets/doctor.svg" alt="doctor-icon" class="doctor-icon" /> -->
+                Doctor :</label>
+              <div>
+                <SearchableDropdown :id="'doctor'" :placeholder="'Doctor Name'" :dataList="doctorsList"/>
+              </div>
+            </div>
+            <div class="section-input">
+              <label for="payment">
+                  <!-- <img src="../../assets/cash.svg" svg-inline alt="cash-icon" class="cash-icon" /> -->
+                Payment :</label>
+              <input type="number" name="payment" id="payment" v-model="payment">
+            </div>
+            <div class="button-group">
+              <button class="cancel-button" @click="$router.push('/')">Cancel</button>
+              <button type="submit" class="submit-button">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -75,4 +109,105 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.add-appt-main {
+  background-color: var(--background-primary);
+  padding: 1rem;
+  margin-top: 30px;
+  border-radius: 0.75rem;
+  border: 1px transparent;
+}
+
+.section-header {
+  font-size: 1.125rem;
+  margin-bottom: 15px;
+}
+
+::v-deep .section-input {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+    label {
+      // width: 100%;
+      font-size: 0.875rem;
+    }
+    input {
+      background: var(--light-gray);
+      border: none;
+      outline: none;
+      width: 200px;
+      width: 100%;
+      padding: 5px 10px;
+      color: var(--input-text-color);
+      border-radius: 5px;
+      margin-top: 5px;
+      &:focus {
+        border-bottom: 1px solid var(--focus-blue);
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+    }
+  }
+
+ ::v-deep #patient, ::v-deep #doctor {
+  width: 200px;
+}
+
+::v-deep .time-input-group {
+  display: flex;
+  input.vdatetime-input {
+    // background: transparent;
+    // border: none;
+    // outline: none;
+    width: 6rem;
+    // font-size: 1.125rem;
+    // padding: 2px 0px;
+    // color: var(--input-text-color);
+    margin-top: 5px;
+  }
+  .vdatetime {
+    &:first-of-type {
+      margin-right: 0.5rem;
+    }
+    &:last-of-type {
+      margin-left: 0.5rem;
+    }
+  }
+}
+
+.button-group {
+  display: flex;
+  margin-top: 30px;
+  .submit-button, .cancel-button {
+    padding: 0.325rem 0.625rem;
+    display: flex;
+    justify-content: center;
+    border: none;
+    outline: none;
+    width: 100px;
+    font-size: 0.75rem;
+    border-radius: 5px;
+    background: var(--button-blue);
+    color: white;
+    // position: sticky;
+    // top: 20px;
+    // right: 0;
+  }
+
+  .cancel-button {
+    margin-left: auto;
+    margin-right: 0.625rem;
+    background-color: var(--light-gray);
+    color: var(--primary);
+    border: 1px solid #ccc;
+  }
+}
+
+::v-deep svg {
+  width: 20px;
+  height: 20px;
+  * {
+    fill: black;
+  }
+}
+</style>
