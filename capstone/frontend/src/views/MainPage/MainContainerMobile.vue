5956089743c84f40adc596ace9089d8087f6945c
OSquiddy <siddiqui.omar21@gmail.com>
2021-09-28 10:50:10 +0530

    Basic chart view in mobile

--- capstone/frontend/src/views/MainPage/MainContainerMobile.vue
+++ capstone/frontend/src/views/MainPage/MainContainerMobile.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="grid-container">
-      <div class="grid-item">Duty Schedule</div>
+    <div class="grid-container" v-if="menu">
+      <div class="grid-item" @click="toggleMenu">Overview</div>
       <router-link to="/appointments">
         <div class="grid-item">Appointments</div>
       </router-link>
@@ -13,14 +13,37 @@
       </router-link>
       <div class="grid-item">Dev Support</div>
     </div>
+    <div v-else>
+      Overview Section <span @click="toggleMenu"> Back </span>
+      <div class="total-earnings tile">
+        <div class="total-earnings-header">
+          <img src="../../assets/income.svg" alt="income-icon">
+          Earnings
+        </div>
+        <BarChart />
+      </div>
+        <div class="earnings tile">
+          <div class="earnings-header">Total Earnings</div>
+          <EarningsChart />
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BarChart from '../../components/EarningsBarChart.vue'
+import EarningsChart from '../../components/EarningsLineChart.vue'
 export default {
+  components: { BarChart, EarningsChart },
   name: 'MainContainerMobile',
   data() {
     return {
+      menu: true
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.menu = !this.menu
     }
   }
 }
@@ -44,5 +67,9 @@ export default {
     align-items: center;
     justify-content: center;
   }
+}
+
+.tile {
+  height: 150px;
 }
 </style>
