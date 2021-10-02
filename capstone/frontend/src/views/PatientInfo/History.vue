897fdbcdfdc401ab7c01ceb5252378644e3b5963
OSquiddy <siddiqui.omar21@gmail.com>
2021-10-03 00:33:50 +0530

    Updated History.vue

--- capstone/frontend/src/views/PatientInfo/History.vue
+++ capstone/frontend/src/views/PatientInfo/History.vue
@@ -4,53 +4,99 @@
       <section class="past-history">
         <h2 class="section-header">Past History</h2>
         <h5 class="minor-heading">Allergies</h5>
-        <div class="allergies-group section-input-group">
+        <div class="allergies-group section-input-group" v-if="edit">
           <div class="allergy-input">
-            <label for="allergy1">Allergy 1: </label>
-            <input type="text" name="allergy1" id="allergy1">
-          </div>
-          <div class="allergy-input">
-            <label for="allergy2">Allergy 2:</label>
-            <input type="text" name="allergy2" id="allergy2">
-          </div>
-          <div class="allergy-input">
-            <label for="allergy3">Allergy 3:</label>
-            <input type="text" name="allergy3" id="allergy3">
+            <label for="allergy1">Allergies: </label>
+            <textarea name="allergy" id="allergy"></textarea>
           </div>
         </div>
-        <div class="section-input-group">
+        <ul class="allergies" v-else>
+          <li class="allergy" v-for="allergy in allergies" :key="allergy">
+            {{allergy}}
+          </li>
+        </ul>
           <h5 class="minor-heading" for="general">General</h5>
+        <div class="section-input-group" v-if="edit">
           <textarea name="general" id="general" cols="90" rows="4"></textarea>
         </div>
-        <div class="section-input-group">
+        <div class="common-illnesses">
+          <input type="checkbox" name="t2dm" id="t2dm" :disabled="!edit" :checked="history.t2dm">
+          <label for="t2dm">T2DM</label>
+        </div>
+        <div class="common-illnesses">
+          <input type="checkbox" name="heart_disease" id="heart_disease" :disabled="!edit" :checked="history.heart_disease">
+          <label for="heart_disease">Heart Disease</label>
+        </div>
+        <div class="common-illnesses">
+          <input type="checkbox" name="hypothyroidism" id="hypothyroidism" :disabled="!edit" :checked="history.hypothyroidism">
+          <label for="hypothyroidism">Hypothyroidism</label>
+        </div>
+        <div class="common-illnesses">
+          <input type="checkbox" name="kidney_disease" id="kidney_disease" :disabled="!edit" :checked="history.kidney_disease">
+          <label for="kidney_disease">Chronic Kidney Disease</label>
+        </div>
+        <div class="common-illnesses">
+          <input type="checkbox" name="cardiovascular_disease" id="cardiovascular_disease" :disabled="!edit" :checked="history.cardiovascular_disease">
+          <label for="cardiovascular_disease">Cardiovascular Disease</label>
+        </div>
+        <div class="general" v-if="!edit">
+          {{history.general}} There was no general info
+        </div>
+        <div class="section-input-group" v-if="edit">
           <h5 for="surgical" class="minor-heading">Surgical History</h5>
           <textarea name="surgical" id="surgical" cols="90" rows="4"></textarea>
+        </div>
+        <div class="surgeries" v-if="!edit && history.surgeries">
+          {{history.surgeries}} are not present
         </div>
       </section>
       <section class="family-history">
         <h2 class="section-header">Family History</h2>
-        <div class="famIllness-group section-input-group">
+        <div class="famIllness-group section-input-group" v-if="edit">
           <div class="famIllness-input">
-            <label for="famIllness1">Illness 1: </label>
-            <input type="text" name="famIllness1" id="famIllness1">
-          </div>
-          <div class="allergy-input">
-            <label for="famIllness2">Illness 2:</label>
-            <input type="text" name="famIllness2" id="famIllness2">
-          </div>
-          <div class="allergy-input">
-            <label for="famIllness3">Illness 3:</label>
-            <input type="text" name="famIllness3" id="famIllness3">
+            <label for="famIllness">Illnesses: </label>
+            <textarea name="famIllness" id="famIllness"></textarea>
           </div>
         </div>
+        <ul class="allergies" v-else>
+          <li class="allergy" v-for="allergy in allergies" :key="allergy">
+            {{allergy}}
+          </li>
+        </ul>
       </section>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
-  name: 'History'
+  name: 'History',
+  data() {
+    return {
+      history: {
+        general: null,
+        surgeries: null
+      },
+      allergies: [],
+      familyIllnesses: [],
+      edit: false
+    }
+  },
+  computed: {
+    ...mapState(['patient'])
+  },
+  mounted() {
+    this.getPatientHistory()
+  },
+  methods: {
+    async getPatientHistory() {
+      const response = await axios.get(process.env.VUE_APP_API_URL + '/history/' + this.patient.id)
+      this.history = response.data.history
+      this.history.allergies.split(/\r?\n/).map((line) => { this.allergies.push(line) })
+    }
+  }
 }
 </script>
 
@@ -113,11 +159,14 @@ section {
   }
 }
 
-.past-history, .family-history {
-  .allergies-group, .famIllness-group {
+.past-history,
+.family-history {
+  .allergies-group,
+  .famIllness-group {
     // display: flex;
     // flex-direction: column;
-    .allergy-input, .famIllness-input {
+    .allergy-input,
+    .famIllness-input {
       // width: fit-content;
       justify-content: space-between;
       // border: 1px solid;
