5805f644efa7faa8d1dcdb4084e1e683f61e3c20
OSquiddy <siddiqui.omar21@gmail.com>
2021-10-02 19:28:43 +0530

    Changed email to username on login page

--- capstone/frontend/src/components/LoginPage.vue
+++ capstone/frontend/src/components/LoginPage.vue
@@ -2,17 +2,12 @@
   <div class="container">
     <form class="text-left" @submit.prevent="submitForm">
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email" />
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label for="username" class="form-label">Username</label>
+        <input type="text" class="form-control" id="username" v-model="username" />
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
         <input type="password" class="form-control" id="exampleInputPassword1" v-model="password" />
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
@@ -35,7 +30,7 @@ export default {
       tabs: ['doctor', 'patient'],
       form: false,
       user: '',
-      email: '',
+      username: '',
       password: '',
       errors: []
     }
@@ -58,9 +53,9 @@ export default {
       axios.defaults.headers.common['Authorization'] = ''
       localStorage.removeItem('token')
 
-      console.log(this.email)
+      console.log(this.username)
       const formData = {
-        username: this.email,
+        username: this.username,
         password: this.password
       }
       try {
