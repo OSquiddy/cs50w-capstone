467315597594a979e9059f001204f20815735afc
OSquiddy <siddiqui.omar21@gmail.com>
2021-10-23 03:37:47 +0530

    Login Page styling completed

--- capstone/frontend/src/components/LoginPage.vue
+++ capstone/frontend/src/components/LoginPage.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="container">
-    <form class="text-left" @submit.prevent="submitForm">
-      <div class="mb-3">
+  <div class="container-fluid">
+    <form class="text-left" @submit.prevent="submitForm" id="form">
+      <div class="form-header">
+        <svg width="44" height="46" viewBox="0 0 44 46" fill="none" xmlns="http://www.w3.org/2000/svg" class="logo-svg">
+            <rect x="14.5332" y="1" width="14.5329" height="43.5986" fill="#DB1111" />
+            <rect y="15.533" width="43.5986" height="14.5329" fill="#DB1111" />
+            <path d="M14.5327 29.7021L21.4358 1.36302" stroke="white" stroke-width="2" stroke-linecap="round" />
+            <path d="M29.0653 30.0651L21.4355 1.36269" stroke="white" stroke-width="2" stroke-linecap="round" />
+            <path d="M14.5327 15.8958L21.7991 44.2349" stroke="white" stroke-width="2" stroke-linecap="round" />
+            <path d="M29.0644 15.8958L21.7981 44.2349" stroke="white" stroke-width="2" stroke-linecap="round" />
+            <path d="M7.26611 22.0723L36.3318 22.4356" stroke="white" stroke-width="2" stroke-linecap="round" />
+          </svg>
+          <span class="logo-text">Avicenna Hospital</span>
+      </div>
+      <div class="input-container">
+        <div class="form-group floatingLabelField">
+            <input class="form-control" type="text" name="username" id="username" required placeholder="Username" v-model="username">
+            <label for="username" class="label">Username</label>
+        </div>
+        <div class="form-group floatingLabelField">
+            <input class="form-control" type="password" name="password" id="password" required placeholder="Password" v-model="password">
+            <label for="password" class="label">Password</label>
+        </div>
+        <a href="#" class="forgot">Forgot Password?</a>
+        <button type="submit" class="btn">Sign In</button>
+      </div>
+      <!-- <div class="mb-3">
         <label for="username" class="form-label">Username</label>
         <input type="text" class="form-control" id="username" v-model="username" />
       </div>
@@ -9,7 +33,7 @@
         <label for="exampleInputPassword1" class="form-label">Password</label>
         <input type="password" class="form-control" id="exampleInputPassword1" v-model="password" />
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Submit</button> -->
     </form>
   </div>
 </template>
@@ -122,22 +146,111 @@ export default {
 }
 </script>
 
-<style>
-.login-img-container {
-  position: relative;
+<style lang="scss" scoped>
+.container-fluid {
+  display: flex;
+  border: 1px solid;
+  height: 100%;
+  background: var(--light-gray);
 }
 
-.login-img {
+#form {
+    max-width: 300px;
+    background: var(--background-primary);
+    box-shadow: 0 3px 20px 0 rgb(0 0 0 / 10%);
+    border-radius: 5px;
+    overflow: hidden;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+}
+
+::v-deep .form-header {
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  background: var(--primary-accent-dark);
+  .logo-svg {
+    border: black;
+    margin-right: 10px;
+    rect {
+      fill: var(--background-primary);
+    }
+    path {
+      stroke: var(--primary-accent-dark);
+    }
+  }
+  .logo-text {
+    color: var(--background-primary);
+  }
+}
+
+#form .input-container {
+  padding: 50px;
+}
+
+#form .btn {
+  margin-top: 30px;
   width: 100%;
+  background: var(--primary-accent-dark);
+  color: var(--background-primary);
+  &:focus {
+    box-shadow: 0 0 0 0.25rem rgb(198 0 0 / 25%) !important;
+    // background-color: rgb(198 0 0 / 25%);
+  }
 }
 
-.doctor-img-text,
-.patient-img-text {
-  position: absolute;
+.floatingLabelField {
+    position: relative !important;
+    margin-top: 0px;
+    margin-bottom: 7px;
+    padding-bottom: 5px;
+    border-bottom: 1px solid #9e9e9e;
+}
 
-  bottom: 0;
-  left: 10px;
-  /* color: white; */
-  /* text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.7); */
+.floatingLabelField:focus-within {
+    border-bottom: 2px solid #03a9f4;
+}
+
+#form .forgot {
+  display: flex;
+  font-size: 0.75rem;
+  // text-align: right;
+  margin-top: 10px;
+  justify-content: flex-end;
+}
+
+#form .form-control {
+    display: block;
+    width: 100%;
+    padding: 20px 10px 0;
+    border: none;
+    border-radius: 0;
+    padding-bottom: 5px;
+}
+
+#form .form-control:focus {
+    box-shadow: none;
+    border: none;
+    outline: none;
+}
+
+#form .label {
+    position: absolute;
+    pointer-events: none;
+    top: 5px;
+    left: 0px;
+    opacity: 0;
+    font-size: 13px;
+    background-color: transparent;
+    padding: 0 10px;
+    -webkit-transition: 0.2s ease-in-out;
+    transition: 0.2s ease-in-out;
+}
+
+#form .form-control:not(:placeholder-shown)+.label {
+    opacity: 1;
+    top: -2px;
+    color: #03a9f4;
 }
 </style>
