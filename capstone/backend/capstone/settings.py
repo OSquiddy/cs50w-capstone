34234c2b6bb7f8a5be283a1e48785db7e2bf99fd
OSquiddy <siddiqui.omar21@gmail.com>
2026-07-30 09:57:04 -0500

    Docker working for local development - using sqlite

--- capstone/backend/capstone/settings.py
+++ capstone/backend/capstone/settings.py
@@ -16,12 +16,12 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # print('Base dir is', BASE_DIR)
+
+# Load environment variables
+load_dotenv(BASE_DIR / '.env.local')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
